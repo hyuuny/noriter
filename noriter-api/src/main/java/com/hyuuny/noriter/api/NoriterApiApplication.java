9d5912368f364eb29a84fcbdebded1fbcb63d49f
hyuuny <shyune@knou.ac.kr>
2023-03-07 20:38:52 +0900

    feat($compile): 프로젝트 생성 및 설정

---
+++ noriter-api/src/main/java/com/hyuuny/noriter/api/NoriterApiApplication.java
@@ -0,0 +1,13 @@
+package com.hyuuny.noriter.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoriterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoriterApiApplication.class, args);
+    }
+
+}
