4e55b3f3ce0b1227701b151718483b6e00fda3b9
hyuuny <shyune@knou.ac.kr>
2023-03-10 12:01:32 +0900

    feat(common): NoriterApiApplication 설정 추가
    - EnableJpaAuditing
    - scanBasePackages

--- noriter-api/src/main/java/com/hyuuny/noriter/api/NoriterApiApplication.java
+++ noriter-api/src/main/java/com/hyuuny/noriter/api/NoriterApiApplication.java
@@ -2,8 +2,15 @@ package com.hyuuny.noriter.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.hyuuny.noriter.api",
+                "com.hyuuny.noriter.support"
+        }
+)
 public class NoriterApiApplication {
 
     public static void main(String[] args) {
