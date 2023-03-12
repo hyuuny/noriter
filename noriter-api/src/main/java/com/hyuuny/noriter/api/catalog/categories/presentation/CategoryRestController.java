20f54a7dd11c2c45a796fe53aa81afe58b8b4f53
hyuuny <shyune@knou.ac.kr>
2023-03-12 17:51:14 +0900

    feat(category): 회원은 카테고리 목록을 조회할 수 있다.

---
+++ noriter-api/src/main/java/com/hyuuny/noriter/api/catalog/categories/presentation/CategoryRestController.java
@@ -0,0 +1,33 @@
+package com.hyuuny.noriter.api.catalog.categories.presentation;
+
+
+import com.hyuuny.noriter.api.catalog.categories.application.CategoryDto;
+import com.hyuuny.noriter.api.catalog.categories.application.CategoryService;
+import com.hyuuny.noriter.support.common.abstraction.AbstractController;
+import com.hyuuny.noriter.support.common.dto.NoriterResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "카테고리 API")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/categories")
+@RestController
+public class CategoryRestController extends AbstractController {
+
+    private final CategoryService categoryService;
+
+    @Operation(summary = "카테고리 목록 조회")
+    @GetMapping
+    public ResponseEntity<NoriterResponseDto<List<CategoryDto.Response>>> getCategories() {
+        List<CategoryDto.Response> existingCategories = categoryService.getCategories();
+        return ok(existingCategories);
+    }
+
+}
---
+++ noriter-api/src/test/java/com/hyuuny/noriter/api/catalog/categories/presentation/CategoryRestControllerTest.java
@@ -0,0 +1,70 @@
+package com.hyuuny.noriter.api.catalog.categories.presentation;
+
+import com.hyuuny.noriter.api.catalog.categories.application.CategoryDto;
+import com.hyuuny.noriter.api.catalog.categories.application.CategoryService;
+import com.hyuuny.noriter.api.catalog.categories.domain.CategoryRepository;
+import com.hyuuny.noriter.api.common.BaseIntegrationTests;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.util.stream.IntStream;
+
+import static com.hyuuny.noriter.api.Fixtures.aCategory;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+class CategoryRestControllerTest extends BaseIntegrationTests {
+
+    private final String CATEGORY_REQUEST_PATH = "/api/v1/categories";
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @BeforeEach
+    void setUp() {
+        log.info("CategoryRestControllerTest.deleteAll");
+        categoryRepository.deleteAll();
+    }
+
+    @DisplayName("회원은 카테고리 목록을 조회할 수 있다.")
+    @Test
+    void getCategories() throws Exception {
+        IntStream.range(0, 11).forEach(value -> {
+            CategoryDto.Create dto = aCategory()
+                    .name("카테고리" + value)
+                    .priorityNumber(value)
+                    .build();
+            categoryService.createCategory(dto);
+        });
+
+        mockMvc.perform(get(CATEGORY_REQUEST_PATH)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value("20000"))
+                .andExpect(jsonPath("message").value("OK"))
+                .andExpect(jsonPath("data[0].priorityNumber").value(10))
+                .andExpect(jsonPath("data[1].priorityNumber").value(9))
+                .andExpect(jsonPath("data[2].priorityNumber").value(8))
+                .andExpect(jsonPath("data[3].priorityNumber").value(7))
+                .andExpect(jsonPath("data[4].priorityNumber").value(6))
+                .andExpect(jsonPath("data[5].priorityNumber").value(5))
+                .andExpect(jsonPath("data[6].priorityNumber").value(4))
+                .andExpect(jsonPath("data[7].priorityNumber").value(3))
+                .andExpect(jsonPath("data[8].priorityNumber").value(2))
+                .andExpect(jsonPath("data[9].priorityNumber").value(1))
+                .andExpect(jsonPath("data[10].priorityNumber").value(0))
+        ;
+    }
+
+}
